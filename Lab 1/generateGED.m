5f337f6f99693ce63a74a772fed3a5c49da4c412
daehjeon <daehjeon@gmail.com>
2019-02-10 13:33:16 -0500

    GED

--- Lab 1/generateGED.m
+++ Lab 1/generateGED.m
@@ -1,9 +1,30 @@
-function [S_trans, mu_trans] = generateGED(sigma, mu, S)
-%generateGED Transform to generalized Euclidean metric
-diag_cov = diag(eig(sigma)); 
-[V, phi] = eig(sigma);
-W = diag_cov ^ (-0.5) * phi';
-mu_trans = W * mu';
-S_trans = S * W;
-end
+function [distance] = generateGED(sigma1, mu1, sigma2, mu2, x, y, distanceAB)
+    %For class A & B    
+    if distanceAB > 0
+        % Transposing the covariances 
+        invA = inv(sigma1);
+        invN = inv(sigma2);
+        distance = distanceAB;
+        get_dist = @(point, covar, mean) sqrt((point - mean) * inv(covar) * transpose(point - mean));
 
+        % MICD/GED equation
+        for (i=1:size(x,1))
+            for(j=1:size(y,2))
+                xBar = [x(i,j) y(i,j)];
+                distance(i, j) = get_dist(xBar, sigma1, mu1) - get_dist(xBar, sigma2, mu2);
+            end
+        end  
+        
+    %For class C, D, E    
+    else
+        dist = zeros(size(x, 1), size(y, 2));
+        get_dist = @(point, covar, mean) sqrt((point - mean) * inv(covar) * transpose(point - mean));
+
+        for i=1:size(x, 1)
+            for j=1:size(y, 2)
+                xBar = [x(i,j) y(i,j)];
+                distance(i, j) = get_dist(xBar, sigma1, mu1) - get_dist(xBar, sigma2, mu2);
+            end
+        end
+    end
+end
